44f6399f430b342ec6490d6ebfd57e061512c4cc
AlexWilson06 <aw.waiheke@gmail.com>
2024-05-22 10:29:41 +1200

    fixed issues with css
    fixed my issue with css by manually forcing the site to fully reload using Ctrl F5. also removed all other files in order to focus purely on the home page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <title>Propaganda Clicker: Home Page</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="css/style.css">
 <style>
 </style>
 </head>
