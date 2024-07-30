e39ed0919a1bbbc432f2a272403465bdb4133050
AlexWilson06 <aw.waiheke@gmail.com>
2024-07-30 20:44:05 +1200

    media Queries
    got most of a simple reformat with the media queries done, with some work needed on the moblie home page and a fix needed to the desktop feedback sheet.

--- NavBar.php
+++ NavBar.php
@@ -1,19 +1,7 @@
 <div class="navbarC">
-    <!-- Outer links -->
     <a href="index.php">Clicker</a>
     <a href="contacts.php">Feedback</a>
-
-    <!-- Burger Menu Icon (visible only on mobile) -->
-    <div class="burger-menu" id="burgerMenu">
-        <div class="burger-bar"></div>
-        <div class="burger-bar"></div>
-        <div class="burger-bar"></div>
-    </div>
-
-    <!-- Inner links (hidden on mobile) -->
-    <div class="nav-links" id="navLinks">
-        <a href="Statistics.php">Statistics</a>
-        <a href="settings.php">Settings</a>
-        <a href="sign in.php">Sign In</a>
-    </div>
+    <a href="Statistics.php">Statistics</a>
+    <a href="settings.php">Settings</a>
+    <a href="sign in.php">Sign In</a>
 </div>
